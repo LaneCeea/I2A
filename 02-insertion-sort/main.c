65fd10451e44f4be305d1e7e22104461006b0ba4
Lan <ivan9172b@gmail.com>
2025-07-04 16:53:36 +0800

    add 02-insertion-sort time measuring

--- 02-insertion-sort/main.c
+++ 02-insertion-sort/main.c
@@ -1,8 +1,12 @@
+#include "utils/timer.h"
+#include "utils/pcg32.h"
 #include "utils/sarray.h"
 
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 
+static
 void insertion_sort(sarray* arr) {
     for (sarray_size_t i = 1; i < arr->size; ++i) {
         sarray_data_t key = arr->data[i];
@@ -18,16 +22,38 @@ void insertion_sort(sarray* arr) {
     }
 }
 
+static
+bool is_sorted(const sarray* arr) {
+    for (size_t i = 1; i < arr->size; ++i) {
+        if (arr->data[i - 1] > arr->data[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
+    pcg32_seed(666U, 999U);
+    printf("insertion sort time measuring\n");
     sarray arr;
-    size_t size = 0;
-    scanf("%zu", &size);
-    sarray_malloc(&arr, size);
-    sarray_input(&arr);
+    uint32_t size = 1;
+    for (int exp = 0; exp < 32; ++exp) {
+        sarray_malloc(&arr, size);
+        sarray_random(&arr, 0, size);
+        
+        TIMER_INIT(sort);
+        TIMER_START(sort);
+        insertion_sort(&arr);
+        TIMER_STOP(sort);
+        double time = TIMER_ELAPSED_MSEC(sort);
+        printf("size 2^(%2u) - %13.6f ms\n", exp, time);
 
-    insertion_sort(&arr);
+        if (!is_sorted(&arr)) {
+            printf("not sorted!\n");
+        }
 
-    sarray_output(&arr);
-    sarray_free(&arr);
+        sarray_free(&arr);
+        size <<= 1;
+    }
     return 0;
 }
